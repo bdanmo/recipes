6b988197752b5de38d76496a0ff18d6a6266f9c6
Brendan Moran <b.daniel.moran@gmail.com>
2020-11-09 18:47:59 -0500

    set up static server, linked files in layout

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+const { ppid } = require('process');
 
 var app = express();
 
@@ -12,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use('/static', express.static('public'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
